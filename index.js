6bb1576d3731542f1816dbb5b6903345f60cc161
L21A-UI <lateefar2009@gmail.com>
2024-08-27 17:47:39 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Playing VALORANT"];
+const statusMessages = ["Listening to Swrv.it","Playing with dirtyfootball"];
 
 
 let currentIndex = 0;
